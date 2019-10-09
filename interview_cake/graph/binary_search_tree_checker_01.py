da689e92c156ef0b89d91d397e298849e85c1caa
Hyungmo Gu <guhyungm7@gmail.com>
2019-10-09 13:24:26 -0700

    feat: Solve interview cake's 'binary search tree checker'

---
+++ interview_cake/graph/binary_search_tree_checker_01.py
@@ -0,0 +1,47 @@
+# Binary Search Tree Checker
+#
+# Write a function to check that a binary tree is a valid binary search tree.
+#
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+class Solution:
+    def __init__(self):
+        self.current_val = 0
+
+    def binarySearchTreeChecker(self, root):
+        val_list = []
+
+        self.collectInOrder(root, val_list)
+
+        for index in range (1, len(val_list)):
+            if val_list[index - 1] > val_list[index]:
+                return False
+
+        return True
+
+    def collectInOrder(self, node, val_list):
+        if node:
+            self.collectInOrder(node.left, val_list)
+
+            val_list.append(node.val)
+
+            self.collectInOrder(node.right, val_list)
+
+if __name__ == '__main__':
+    case_1 = Node(4)
+    case_1.left = Node(2)
+    case_1.right = Node(5)
+    case_1.left.left = Node(1)
+    case_1.left.right = Node(3)
+
+    expected = True
+
+    solution = Solution().binarySearchTreeChecker(case_1)
+
+    assert solution == expected
