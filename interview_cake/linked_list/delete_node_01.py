7209b16d1cf2799d3144ef6f19204588a2e7896c
Hyungmo Gu <guhyungm7@gmail.com>
2019-10-11 07:16:10 -0700

    feat: Solve interview cake's 'Delete Node'

---
+++ interview_cake/linked_list/delete_node_01.py
@@ -0,0 +1,73 @@
+# Delete Node
+# Given Linked List Node
+
+class LinkedListNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+# Create an algorithm that deletes a node by variable
+# The input could, for example, be the variable b below:
+#
+# a = LinkedListNode('A')
+# b = LinkedListNode('B')
+# c = LinkedListNode('C')
+#
+# a.next = b
+# b.next = c
+#
+# delete_node(b)
+#
+# Input
+#   - LinkListNode
+#
+# Output
+#   - None
+#
+# Example
+#
+#
+# A  -   B  -   C
+#
+# A  -   B  -   C
+# ^     DELETE
+# |
+# a.next = c
+#
+#
+# A - C
+#
+# First Thought
+#   1. before the end, traverse through the linked list
+#   2. if the next node is the same as the variable, then remove the node
+#   3. return None
+
+
+# BUT WAIT!!!! We can't access the previous node. How can we delete node if we cant access it?
+#
+#
+# New Solution
+#   1. Set the value of B the same as C
+#   2. set b.next (now c) to None
+
+def delete_node(node):
+
+    if node.next == None:
+        return
+
+    node.value = node.next.value
+    node.next = None
+
+if __name__ == '__main__':
+    a = LinkedListNode('A')
+    b = LinkedListNode('B')
+    c = LinkedListNode('C')
+
+    a.next = b
+    b.next = c
+
+    delete_node(b)
+
+    assert a.next.value == 'C'
+    assert b.next == None
