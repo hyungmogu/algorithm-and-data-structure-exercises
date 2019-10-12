c08712b979c0d5b4cf89163f0d68050a75be6120
Hyungmo Gu <guhyungm7@gmail.com>
2019-10-12 09:21:40 -0700

    feat: Solve interview cake's 'highest prod of 3'

--- interview_cake/greedy/highest_product_of_3_02.py
+++ interview_cake/greedy/highest_product_of_3_02.py
@@ -55,6 +55,7 @@ class Solution:
     def highestProductOf3(self, list_of_ints):
     #   1. create an array highest_val_list = [third_highest, second_highest, highest] of highest values
         highest_val_list = [None, None, None]
+
     #   2. for each element in array
         for element in list_of_ints:
             #   2.1 if highest_val_list is empty, then add the element to highest
@@ -76,19 +77,40 @@ class Solution:
                 highest_val_list[0] = element
                 continue
 
-
-
+        lowest_product_of_two = self.getLowestProductOfTwo(list_of_ints)
         total_product = functools.reduce(lambda x,y: x*y, highest_val_list)
 
+        if lowest_product_of_two > 0 and lowest_product_of_two * highest_val_list[-1] > total_product:
+            total_product = lowest_product_of_two * highest_val_list[-1]
+
         return total_product
+
+    def getLowestProductOfTwo(self, list_of_ints):
+        lowest_val_list = [None, None]
+
+        for element in list_of_ints:
+            if lowest_val_list[1] == None or element < lowest_val_list[1]:
+                lowest_val_list[0] = lowest_val_list[1]
+                lowest_val_list[1] = element
+                continue
+
+            if lowest_val_list[0] == None or element < lowest_val_list[0]:
+                lowest_val_list[0] = element
+
+        return functools.reduce(lambda x,y: x*y, lowest_val_list)
+
 
 if __name__ == '__main__':
 
     case_1 = [9, 1, 5, 16, 2, 3]
+    case_2 = [-10,-10, 3, 1, 5, 2]
 
     expected_1 = 720
+    expected_2 = 500
 
     solution_1 = Solution().highestProductOf3(case_1)
+    solution_2 = Solution().highestProductOf3(case_2)
 
     assert expected_1 == solution_1
+    assert expected_2 == solution_2
 
