4a21960633d2b4257274553ace91df0f593c202d
Hyungmo Gu <guhyungm7@gmail.com>
2019-10-14 13:00:28 -0700

    feat: Solve interview cake's 'make change'

--- interview_cake/dynamic_programming/make_change_01.py
+++ interview_cake/dynamic_programming/make_change_01.py
@@ -21,5 +21,37 @@
 # 2¢, 2¢
 #
 #
-#
-# Comment: Dynamic programming is hard!! 으허허허헝
+
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def makeChange(self, coins, coin_index, amount):
+        if amount == 0:
+            return 1
+
+        if amount < 0:
+            return 0
+
+        if coin_index <= 0:
+            return 0
+
+        if '{} {}'.format(coin_index, amount) in self.memo:
+            return self.memo['{} {}'.format(coin_index, amount)]
+
+        numChanges = self.makeChange(coins, coin_index - 1, amount) + self.makeChange(coins, coin_index, amount - coins[coin_index - 1])
+
+        self.memo['{} {}'.format(coin_index, amount)] = numChanges
+
+        return numChanges
+
+
+if __name__ == "__main__":
+    amount_1 = 4
+    coins_1 = [1,2,3]
+    coin_index_1 = len(coins_1)
+
+    expected_1 = 4
+
+    solution_1 = Solution().makeChange(coins_1, coin_index_1, amount_1)
+
+    assert solution_1 == expected_1
