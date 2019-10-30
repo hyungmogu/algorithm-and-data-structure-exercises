575561b54dd83956ce1b56abbf1bf709608a6271
Hyungmo Gu <guhyungm7@gmail.com>
2019-10-30 16:48:38 -0700

    feat: solve codility's  'number solitaire' 2nd time

---
+++ codility_practice/dynamic_programming/number_solitaire_02.py
@@ -0,0 +1,27 @@
+# number solitaire
+#
+# =============== solution ===================
+#
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+#
+# 1. find dp of first 6 moves
+# 2. find dp until index == N
+# 3. return dp
+def solution(A):
+    index = 1
+    N = len(A)
+    dp = [A[0]] + [0] * (N-1)
+
+    # 1. find dp of first 5 moves
+    while index < min(7,N):
+        dp[index] = A[index] + max(dp[:index])
+        index += 1
+
+    # 2. find dp until index == N
+    while index < N:
+        dp[index] = A[index] + max(dp[index-6:index])
+        index += 1
+
+    # 3. return dp
+    return dp[-1]
