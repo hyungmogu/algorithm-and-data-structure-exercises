cd883623ac4035b8df135cfa9f6cbe3656fdd64f
Hyungmo Gu <guhyungm7@gmail.com>
2019-11-09 09:54:16 -0800

    feat: solve codility's 'tie ropes' 3rd time

---
+++ codility_practice/greedy/tie_ropes_03.py
@@ -0,0 +1,130 @@
+# Tie Ropes
+# https://app.codility.com/programmers/lessons/16-greedy_algorithms/tie_ropes/
+#
+# There are N ropes numbered from 0 to N − 1, whose lengths are given in an
+# array A, lying on the floor in a line. For each I (0 ≤ I < N), the length of
+# rope I on the line is A[I].
+#
+# We say that two ropes I and I + 1 are adjacent. Two adjacent ropes can be tied
+# together with a knot, and the length of the tied rope is the sum of lengths of
+# both ropes. The resulting new rope can then be tied again.
+#
+# For a given integer K, the goal is to tie the ropes in such a way that the
+# number of ropes whose length is greater than or equal to K is maximal.
+#
+# For example, consider K = 4 and array A such that:
+#
+#     A[0] = 1
+#     A[1] = 2
+#     A[2] = 3
+#     A[3] = 4
+#     A[4] = 1
+#     A[5] = 1
+#     A[6] = 3
+# The ropes are shown in the figure below.
+#
+#
+#
+# We can tie:
+#
+# rope 1 with rope 2 to produce a rope of length A[1] + A[2] = 5;
+# rope 4 with rope 5 with rope 6 to produce a rope of length A[4] + A[5] + A[6] = 5.
+# After that, there will be three ropes whose lengths are greater than or equal
+# to K = 4. It is not possible to produce four such ropes.
+#
+# Write a function:
+#
+# def solution(K, A)
+#
+# that, given an integer K and a non-empty array A of N integers, returns the
+# maximum number of ropes of length greater than or equal to K that can be
+# created.
+#
+# For example, given K = 4 and array A such that:
+#
+#     A[0] = 1
+#     A[1] = 2
+#     A[2] = 3
+#     A[3] = 4
+#     A[4] = 1
+#     A[5] = 1
+#     A[6] = 3
+# the function should return 3, as explained above.
+#
+# Write an efficient algorithm for the following assumptions:
+#
+# N is an integer within the range [1..100,000];
+# K is an integer within the range [1..1,000,000,000];
+# each element of array A is an integer within the range [1..1,000,000,000].
+#
+#
+# =================== solution ======================
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+#
+#
+# goal
+#   - given integer K, and non emptry array A of N integers, return the maximum number of ropes of length greater than or equal to K that can be created
+#
+# input
+#   - Integer K
+#   - list of integers A
+#
+# output
+#   - integer (maximum number of ropes with value greater than or equal to K)
+#
+# constraint
+#   - N is an integer in range [1 ... 100,000]
+#   - K is an integer in range [1 ... 1,000,000,000]
+#
+# known
+#   - only adjancent ropes can be tied
+#   - use greedy algorithm
+#
+# brute force solution
+#   1. Try all different possible combinations and return the maximum
+#
+#   Time complexity O(N^N), spatial complexity O(1) --> Not cool
+#
+# cases
+#   1. K == 1 --> return N
+#
+# Improved version of solution (Using greedy algorithm)
+#   1. while index < N where index = 1
+#   2. if rope_sum + A[index] < K, add to rope_sum
+#   3. if rope_sum + A[index] >= K, increment number of ropes, and set rope_sum to be 0
+#   4. return num_ropes
+
+#
+# Time complexity O(N), spatial complexity O(1)
+
+def solution(K, A):
+    index = 1
+    rope_sum = A[0]
+    num_ropes = 0
+    N = len(A)
+
+    if K == 1:
+        return N
+
+    if A[0] >= K:
+        num_ropes += 1
+        rope_sum = 0
+
+    #   1. while index < N where index = 1
+    while index < N:
+        #   2. if rope_sum + A[index] < K, add to rope_sum
+        if rope_sum + A[index] < K:
+            rope_sum += A[index]
+        #   3. if rope_sum + A[index] >= K, increment number of ropes, and set rope_sum to be A[index]
+        else:
+            rope_sum = 0
+            num_ropes += 1
+        index += 1
+
+    #   4. return num_ropes
+    return num_ropes
+
+
+# self feedback
+#   - Need to calm down. All will be well.
